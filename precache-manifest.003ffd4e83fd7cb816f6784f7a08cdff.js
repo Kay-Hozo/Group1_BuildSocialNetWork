6cc2c1b57c1e62b6a013a51fcf022d1c806ade52
Trần Khánh Linh <ktpmlinh2002@gmail.com>
2023-03-09 21:59:10 +0700

    Updates

---
+++ precache-manifest.003ffd4e83fd7cb816f6784f7a08cdff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed58d9ed415ee4176ad",
+    "url": "/Group1_BuildSocialNetWork/static/css/main.d899213f.chunk.css"
+  },
+  {
+    "revision": "eed58d9ed415ee4176ad",
+    "url": "/Group1_BuildSocialNetWork/static/js/main.eed58d9e.chunk.js"
+  },
+  {
+    "revision": "4f22371c99f1c26d03c2",
+    "url": "/Group1_BuildSocialNetWork/static/css/1.4ca38be0.chunk.css"
+  },
+  {
+    "revision": "4f22371c99f1c26d03c2",
+    "url": "/Group1_BuildSocialNetWork/static/js/1.4f22371c.chunk.js"
+  },
+  {
+    "revision": "fdd247f0425f9a6cc415",
+    "url": "/Group1_BuildSocialNetWork/static/js/runtime~main.fdd247f0.js"
+  },
+  {
+    "revision": "914e10a93fa59e9657e8e78d9d8705c6",
+    "url": "/Group1_BuildSocialNetWork/index.html"
+  }
+];
